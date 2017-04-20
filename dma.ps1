db3596112859225a9f62af96ed43be5cf5ead4f3
Cláudio Silva <claudiosil100@gmail.com>
2017-04-20 11:03:47 +0100

    v0.1 - Automating Data Migration Assistance
    This allow to run the SQLServer DMA based on text file with Server-Database per line.
    The output will create a folder with Instance name and a folder per database
    Example:
    C:\temp\inst1\db1
    C:\temp\inst1\db2
    C:\temp\inst2\db5

---
+++ dma.ps1
@@ -0,0 +1,21 @@
+﻿#& "${env:ProgramFiles}\Microsoft Data Migration Assistant\DmaCmd.exe" /AssessmentName="TestAssessment" /AssessmentDatabases="Server=SQL2000;Initial Catalog=pubs;Integrated Security=true" /AssessmentTargetPlatform="SqlServer2016" /AssessmentEvaluateCompatibilityIssues /AssessmentOverwriteResult /AssessmentResultCsv="C:\temp\Results\AssessmentReport2000.csv" /AssessmentResultJson="C:\temp\results\test2000.json"
+$servers = Import-Csv "C:\Csilva\OneDrive\Git\dma\servers.txt" -Delimiter "`t" #tab
+
+$resultFolder = "C:\temp\results"
+
+#passar todos os servidores e bases de dados da lista
+foreach ($srv in $servers) {
+    $instance = $srv.Server
+    $database = $srv.Database
+
+    $fileDate = Get-Date -Format "yyyymmdd-HHmmss"
+
+    $resultFileName = "$($database.replace('\','_'))_$fileDate"
+    $resultFullPath = "$resultFolder\$($instance.replace('\','_'))\$($database.replace('\','_'))"
+    
+    New-Item -Path $resultFullPath -ItemType Directory -Force
+
+    & "${env:ProgramFiles}\Microsoft Data Migration Assistant\DmaCmd.exe" /AssessmentName="TestAssessment" /AssessmentDatabases="Server=$instance;Initial Catalog=$database;Integrated Security=true" /AssessmentTargetPlatform="SqlServer2016" /AssessmentEvaluateCompatibilityIssues /AssessmentOverwriteResult /AssessmentResultCsv="$resultFullPath\$resultFileName.csv" /AssessmentResultJson="$resultFullPath\$resultFileName.json"
+}
+
+
