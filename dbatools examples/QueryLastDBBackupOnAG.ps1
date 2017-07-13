f559c9cb5e23c8ae03fb18a1a4f9d811c649b2d5
Cláudio Silva <claudiosil100@gmail.com>
2017-07-13 14:12:49 +0100

    Remove Foreach-object loop

--- dbatools examples/QueryLastDBBackupOnAG.ps1
+++ dbatools examples/QueryLastDBBackupOnAG.ps1
@@ -1,10 +1,7 @@
 #This works if you cann connect to your replica name
 $sqlInstance = "<LISTENER>or<One of the instances - if only one AG>"
 $database = "<DBNAME>"
+
 $AGInfo = Get-DbaAvailabilityGroup -SqlInstance $sqlInstance -
-
-$listBackups = $AGInfo | ForEach-Object {
-	Get-DbaBackupHistory -SqlInstance $_.ReplicaName -Database $database -Last
-}
-
+$listBackups = Get-DbaBackupHistory -SqlInstance $AGInfo.ReplicaName -Database $database -Last
 $listBackups | Sort-Object Start -Descending
