c19258c72da68ccfceaa8935fcec37e374eb48e3
Cláudio Silva <claudiosil100@gmail.com>
2017-08-29 15:10:26 +0100

    Get-MSDocs
    Download pdf files that contains MS documentation

---
+++ Download MS Documentation/Get-MSDocs.ps1
@@ -0,0 +1,47 @@
+$topic = "relational-databases"
+$baseAdress = "https://opbuildstorageprod.blob.core.windows.net/output-pdf-files/en-us/SQL.sql-content/live/$topic"
+$gitHubRepo = "https://github.com/MicrosoftDocs/sql-docs/tree/live/docs/$topic"
+
+$outputDirectory = "d:\ToDelete\MSDocs"
+
+if (!(Test-Path $outputDirectory)) {
+    Write-Warning "Output directory $outputDirectory does not exists."
+    return
+}
+
+$response = Invoke-WebRequest $gitHubRepo
+$webclient = New-Object System.Net.WebClient
+
+Write-Output "Downloading root pdf file '$topic.pdf' from $baseAdress.pdf to $outputDirectory"
+
+#Download base file - current $topic value
+$webclient.DownloadFile("$baseAdress.pdf", "$outputDirectory\$topic.pdf")
+
+# Get all TOC e folder names (excluding 'media '). The folder name is the pdf file name
+$fileName = $response.AllElements | Where-Object { $_.class -eq "content" -and $_.innerHTML -like "*href*" -and $_.outerText -notlike "*.md*" -and $_.outerText -notin ("media ")} | Select-Object -ExpandProperty outerText
+
+if ($fileName.Count -gt 0) {
+    #Number of files to iterate (helps with the progress bar)
+    $numberFiles = $fileNames.Count
+
+    for ($i=0; $i -lt $numberFiles; $i++)
+    {
+        $currentFile = $i + 1
+        $file = "$($fileName[$i].Trim()).pdf"
+        $url = "$baseAdress/$file"
+        $fileDestination = "$outputDirectory\$file"
+
+        Write-Progress -Activity "Downloading files" -Status "Downloading ($currentFile of $numberFiles) - $file" -PercentComplete (($i/$numberFiles)*100)
+
+        try {
+            $webclient.DownloadFile($url, $fileDestination)
+        }
+        catch {
+            Write-Warning "Error downloading file $file from URL: $url."
+        }
+
+    }
+}
+else {
+    Write-Output "No sub folders with files do download."
+}
