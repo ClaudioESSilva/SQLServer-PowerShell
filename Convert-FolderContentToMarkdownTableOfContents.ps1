d40d49350e4ac446d698031fbc00785c6eb944cd
Jaap Brasser <noreply@jaapbrasser.com>
2017-09-18 17:21:21 +0200

    Added uri escape, wrote help

--- Convert-FolderContentToMarkdownTableOfContents.ps1
+++ Convert-FolderContentToMarkdownTableOfContents.ps1
@@ -1,4 +1,27 @@
 function Convert-FolderContentToMarkdownTableOfContents{
+<#
+.SYNOPSIS
+Create a Table of Contents in markdown
+
+.DESCRIPTION
+This function can be used to generate a markdown file that contains a Table of Contents based on the contents of a folder
+
+.PARAMETER BaseFolder
+It’s the folder’s location on the disk
+
+.PARAMETER BaseURL
+to build the URL for each file. This will be added as a link
+
+.PARAMETER FiletypeFilter
+to filter the files on the folder
+
+.EXAMPLE
+Convert-FolderContentToMarkdownTableOfContents -BaseFolder "D:\Github\<module folder>" -BaseURL "https://github.com/<user>/<repository>/tree/master" -FiletypeFilter "*.md"
+
+.NOTES
+https://claudioessilva.eu/2017/09/18/generate-markdown-table-of-contents-based-on-files-within-a-folder-with-powershell/
+#>    
+    
     param (
         [string]$BaseFolder,
         [string]$BaseURL,
@@ -8,7 +31,7 @@ function Convert-FolderContentToMarkdownTableOfContents{
     $nl = [System.Environment]::NewLine
     $TOC = "## Index$nl"
  
-    $repoFolderStructure = Get-ChildItem -Path $BaseFolder -Directory | Where-Object Name -NotLike "*.github*"
+    $repoFolderStructure = Get-ChildItem -Path $BaseFolder -Directory | Where-Object Name -NotMatch "\.github|\.git"
  
     foreach ($dir in ($repoFolderStructure | Sort-Object -Property Name)) {
         $repoStructure = Get-ChildItem -Path $dir.FullName -Filter $FiletypeFilter
@@ -18,11 +41,9 @@ function Convert-FolderContentToMarkdownTableOfContents{
         foreach ($md in ($repoStructure | Sort-Object -Property Name)) {
             $suffix = $($md.Directory.ToString().Replace($BaseFolder, [string]::Empty)).Replace("\", "/")
             $fileName = $($md.Name.TrimEnd($md.Extension))
-            $TOC += "  * [$fileName]($baseURL$suffix/$($md.Name))$nl"
+            $TOC += "  * [$fileName]($([uri]::EscapeUriString(""$baseURL$suffix/$($md.Name)"")))$nl"
         }
     }
  
     return $TOC
 }
- 
-Convert-FolderContentToMarkdownTableOfContents -BaseFolder "D:\Github\<module folder>" -BaseURL "https://github.com/<user>/<repository>/tree/master" -FiletypeFilter "*.md"
