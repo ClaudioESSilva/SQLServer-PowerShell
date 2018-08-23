eb863ac7f12d5e88445c7bbef804c9c5a4a502c5
Cláudio Silva <claudiosil100@gmail.com>
2018-08-23 17:09:58 +0100

    Example_Set-Expression.ps1

---
+++ PowerShell/Set-Expression/Example_Set-Expression.ps1
@@ -0,0 +1,7 @@
+$replacements = [ordered]@{
+    #starting <value> = LIKE '<value>%'
+    'starting([ ]{1,})(''\w{1,})'              = "LIKE`$1`$2%"
+    #starting with '<value>' = LIKE '<value>%'
+    'starting([ ]{1,})with([ ]{1,})(''\w{1,})' = "LIKE`$2`$3%"
+}
+Set-Expression -Pattern $replacements -Multiline -Overwrite -Encoding "UTF8" -FullName "C:\temp\Set-Expression\View.sql"
