acd145dc49ab6f244e38106e6cda45a1601314d0
Cláudio Silva <claudiosil100@gmail.com>
2019-11-28 00:28:24 +0000

    add #05 recipe

--- Presentations/SQLSaturday#926 - Lisboa 2019/localhost,1433/2-customererrors.sql
+++
@@ -1,8 +0,0 @@
-﻿/*
-	Created by REDGLUEL1\Claudio Silva using dbatools Export-DbaScript for objects on localhost,1433 at 11/26/2019 12:33:08
-	See https://dbatools.io/Export-DbaScript for more information
-*/
-EXEC master.dbo.sp_addmessage @msgnum=60000, @lang=N'us_english', 
-		@severity=16, 
-		@msgtext=N'The item named %s already exists in %s.', 
-		@with_log=false
