736295bf9c2447c4305e88710d4f3fecb4f3ef9a
Cláudio Silva <claudiosil100@gmail.com>
2020-07-15 08:39:13 +0100

    Added PASS Marathon Portuguese 2020 slides and demos

---
+++ Presentations/PASS Marathon Portuguese 2020/Replicate Permissions/CheckPermissions/01_CheckPermissions.sql
@@ -0,0 +1,16 @@
+USE Northwind
+GO
+
+SELECT ISNULL(OBJECT_NAME(major_id),'') [Objects], USER_NAME(grantee_principal_id) as [UserName], permission_name as [PermissionName]
+FROM sys.database_permissions p
+WHERE grantee_principal_id>0
+ORDER BY OBJECT_NAME(major_id), USER_NAME(grantee_principal_id), permission_name
+
+USE master
+GO
+SELECT pr.principal_id, pr.name, pr.type_desc,   
+    pe.state_desc, pe.permission_name   
+FROM sys.server_principals AS pr   
+JOIN sys.server_permissions AS pe   
+    ON pe.grantee_principal_id = pr.principal_id
+WHERE pr.name IN ('storageuser', 'storageuserColleague'); 
---
+++ Presentations/PASS Marathon Portuguese 2020/Reset/02_GrantPermissions.sql
@@ -0,0 +1,35 @@
+-- Login permissions
+USE [master]
+GO
+GRANT VIEW ANY DATABASE TO [storageuser]
+GO
+DENY SHUTDOWN TO [storageuser]
+GO
+
+-- User level
+USE [Northwind]
+GO
+CREATE USER [storageuser] FOR LOGIN [storageuser] WITH DEFAULT_SCHEMA=[dbo]
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [storageuser]
+GO
+use [Northwind]
+GO
+GRANT SHOWPLAN TO [storageuser]
+GO
+use [Northwind]
+GO
+GRANT INSERT ON [dbo].[Categories] TO [storageuser]
+GO
+use [Northwind]
+GO
+GRANT SELECT ON [dbo].[Categories] TO [storageuser]
+GO
+use [Northwind]
+GO
+DENY SELECT ON [dbo].[Employees] TO [storageuser]
+GO
+use [Northwind]
+GO
+GRANT SELECT ON [dbo].[Customers] TO [storageuser]
+GO
